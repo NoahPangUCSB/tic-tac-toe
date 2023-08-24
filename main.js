b0383d065b4f2e83ab53d2dd6d1ad6322c85472b
Noah Pang <noahpang@cs.ucsb.edu>
2023-08-24 13:27:40 -0700

    added functionality for inputting player names and displaying whose turn it is

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const BOARD_SIZE = 3;
 
 const displayController = (() => {
+    
     const _createGrid = (() => {
         const gameboard = document.querySelector(".gameboard");
         for(let i = 0; i < BOARD_SIZE**2; i++) {
@@ -10,6 +11,27 @@ const displayController = (() => {
             gameboard.appendChild(gbSquare);
         }
     })();
+
+    const displayPlayerName = (playerDiv, playerName, formInput) => {
+        formInput.style.display = "none";
+        const playerNameDiv = document.createElement("div");
+        playerNameDiv.classList.add("player-name");
+        playerNameDiv.textContent = playerName;
+        playerDiv.appendChild(playerNameDiv);
+    }
+
+    const displayCurrPlayer = (currPlayer, player1) => {
+        const player1Div = document.querySelector(".player1-div");
+        const player2Div = document.querySelector(".player2-div");
+        if(currPlayer === player1) {
+            player1Div.classList.remove("curr-player");
+            player2Div.classList.add("curr-player");
+        } else {
+            player2Div.classList.remove("curr-player");
+            player1Div.classList.add("curr-player");
+        }
+    }
+    
 
     const displayBoard = () => {
         const board_display = document.querySelector(".gameboard");
@@ -37,7 +59,48 @@ const displayController = (() => {
         }
     }
 
-    return {displayBoard};
+    return {displayBoard, displayPlayerName, displayCurrPlayer};
+})();
+
+const Game = (() => {
+    let _player1 = null;
+    let _player2 = null;
+    let _currPlayer = null;
+
+    const getPlayer1 = () => _player1;
+    const getPlayer2 = () => _player2;
+
+    const _setCurrPlayer = () => {
+        displayController.displayCurrPlayer(_currPlayer, _player1);
+        _currPlayer === _player1 ? _player2 : _player1;
+    }
+
+    const _submitPlayerName = (e) => {
+        if(e.key === "Enter") {
+            e.preventDefault();
+            e.srcElement.blur();
+            const playerName = e.srcElement.value;
+            const playerDiv = e.srcElement.parentNode.parentNode;
+            if(playerDiv.className === "player1-div") {
+                _player1 = Player(playerName, "x");
+            } else {
+                _player2 = Player(playerName, "o");
+            }
+
+            displayController.displayPlayerName(playerDiv, playerName, e.srcElement);
+
+            if(_player1 !== null && _player2 !== null) {
+                _setCurrPlayer();
+            }
+        }
+    }
+
+    const _player1Form = document.getElementById("player1-name");
+    const _player2Form = document.getElementById("player2-name");
+    _player1Form.addEventListener("keypress", _submitPlayerName);
+    _player2Form.addEventListener("keypress", _submitPlayerName);
+
+    return {getPlayer1, getPlayer2}
 })();
 
 
